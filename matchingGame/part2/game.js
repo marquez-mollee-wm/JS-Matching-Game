f8fe746f049ccb7ab28befa2dce9ea0a8358e05d
Mollee <session1@START-Coding-MBP-14.local>
2016-08-15 15:19:36 -0700

    Mollee Marquez- I'm stuck on part 2

--- matchingGame/part2/game.js
+++ matchingGame/part2/game.js
@@ -1,3 +1,11 @@
-/**
- * Created by session1 on 8/12/16.
- */
+
+var numberOfFaces = 5;
+var theLeftSide = document.getElementById("leftSide");
+
+function generateFaces(){
+    var img = document.createElement("img.src='pics2/nerdEmoji.png'");
+
+    
+
+
+}
