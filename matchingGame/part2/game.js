8667fcff5cf240b9968feb50db6b53286602cbb7
Mollee <session1@START-Coding-MBP-14.local>
2016-08-16 15:24:27 -0700

    Mollee Marquez - My code is broken you tried helping me and that didn't work either

--- matchingGame/part2/game.js
+++ matchingGame/part2/game.js
@@ -1,11 +1,34 @@
 
-var numberOfFaces = 5;
-var theLeftSide = document.getElementById("leftSide");
+var numberOfPics = 6;
+//
+//function generateRandom() {
+//    var num = Math.floor(Math.random() * 500);
+//    return num;
+//}
 
-function generateFaces(){
-    var img = document.createElement("img.src='pics2/nerdEmoji.png'");
+function generatePic(){
+    for ( var i = 1; i < numberOfPics; i++ ) {
+        var pic = document.createElement("img");
+        pic.setAttribute('src', 'pics2/nerdEmoji.png');
+        var leftOne = Math.random() * 500 /1;
+        var left= Math.round(leftOne);
+        var topOne = Math.random() * 500 /1;
+        var top= Math.round(topOne);
 
-    
+        var leftScreen = document.getElementById("leftScreen");
+        if(topOne >= 200 && leftOne >= 200){
+            pic.style.top= top+"px";
+            pic.style.left= left+"px";
+            leftScreen.appendChild(pic);
+        }
+       else{
+        numberOfPics = numberOfPics + 1;
+
+        }
+
+
+
+    }
 
 
 }
--- matchingGame/part3/game2.js
+++ matchingGame/part3/game2.js
@@ -1,3 +1,50 @@
-/**
- * Created by session1 on 8/12/16.
- */
+
+//var numberOfPics = 6;
+//
+//function generatePic(){
+//    for ( var i = 1; i < numberOfPics; i++ ) {
+//        var pic = document.createElement("img");
+//        pic.setAttribute('src', 'pics2/nerdEmoji.png');
+//        var leftOne = Math.random() * 500 /1;
+//        var left= Math.round(leftOne);
+//        var topOne = Math.random() * 500 /1;
+//        var top= Math.round(topOne);
+//
+//        var leftScreen = document.getElementById("leftScreen");
+//        if(topOne >= 200 && leftOne >= 200){
+//            pic.style.top= top+"px";
+//            pic.style.left= left+"px";
+//            leftScreen.appendChild(pic);
+//        }
+//        else{
+//            numberOfPics = numberOfPics + 1;
+//
+//        }
+//
+//
+//
+//    }
+//
+//    var theRightSide = document.getElementById("rightScreen");
+//    pic = leftScreen.cloneNode(true);
+//    pic.removeChild(leftSideImages.lastChild);
+//    theRightSide.appendChild(pic);
+//
+//}
+
+
+var leftScreen = document.getElementById('leftScreen');
+var rightScreen = document.getElementById('rightScreen');
+var numberOfPics = 5;
+function generatePic() {
+    for (var i = 0; i < numberOfPics; i++) {
+        var pic = document.createElement('img');
+        pic.src = "pics3/nerdEmoji.png";
+        pic.style.top = Math.random() * 400 + "px";
+        pic.style.left = Math.random() * 400 + "px";
+        leftScreen.appendChild(pic);
+    }
+    var leftSideImages = leftScreen.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.childNodes[leftSideImages.childNodes.length - 1]);
+    rightScreen.appendChild(leftSideImages);
+}
